055bc87387d1ebc3a40ec9d217b68a85983a1182
miil <oleg@gmx.tm>
2018-07-03 01:42:41 +0200

    Add files via upload

---
+++ IROleg.ino/IROleg-send.ino/IROleg-send.ino.ino
@@ -0,0 +1,19 @@
+
+
+#include <IRremote.h>
+
+IRsend irsend3;
+
+void setup()
+{
+}
+
+void loop() {
+	for (int i = 0; i < 3; i++) {
+		irsend3.sendSony(0x20DFC03F, 3);
+    irsend3.sendSony(0xFFFFFFFF, 3);
+    irsend3.sendSony(0xFFFFFFFF , 3);
+		delay(40);
+	}
+	delay(5000); //5 second delay between each signal burst
+}
---
+++ IROleg.ino/IROleg.ino.ino
@@ -0,0 +1,34 @@
+/*
+ * IRremote: irrecv3Demo - demonstrates receiving IR codes with IRrecv
+ * An IR detector/demodulator must be connected to the input RECV_PIN.
+ * http://arcfn.com
+ */
+/* lg55 
+	
+  20DF3EC1 - home
+  20DF40BF - v+
+  20DFC03F - v-
+  FFFFFFFF - more last command
+
+ */
+#include <IRremote.h>
+
+//int RECV_PIN = 11;
+
+IRrecv irrecv3(11);
+
+decode_results res3;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv3.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+  if (irrecv3.decode(&res3)) {
+    Serial.println(res3.value, HEX);
+    irrecv3.resume(); // Receive the next value
+  }
+  delay(100);
+}
